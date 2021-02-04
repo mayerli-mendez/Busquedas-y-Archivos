2114bcc5be7002539d32eca00271a361046eb9f2
Mayerli Méndez <mayerli14paredes@gmail.com>
2021-02-04 16:09:49 +0000

    Ejercicio

--- main.cpp
+++ main.cpp
@@ -11,15 +11,21 @@ int dato;
 
 int main() {
 
-
-  int arr[11]={1,2,3,4,5,6,7,8,9,10};
-  
+  int arr[]={2,3,5,1,7,8,10,4,6,9};
+  ofstream binario;
+  binario.open("binario.txt", ios::app);
   cout <<"\nEl tamaño del arreglo es : "<<tamanio(arr);
   cout<<endl;
 
+  binario<<"\nEl tamaño del arreglo es : "<<tamanio(arr);
+  binario<<endl;
+
   int t=tamanio(arr);
-   cout<<"Ingrese el numero a buscar del 1-10 : ";
+  cout<<"\nIngrese el numero a buscar del 1-10 : ";
   cin>>dato;
+  
+  binario<<"\nNumero ingresado por el usuario : "<<dato; 
+  binario<<endl;
   if (dato<=10)
   {
     algBurbuja(arr,t);
@@ -28,13 +34,9 @@ int main() {
 
   if (dato>10)
   {
-    cout<<"No hay es elemento ";
+    cout<<"\nNo hay es elemento "<<endl;
+    binario<<"\nNo hay ese elemento ";
   }
-
-
-
-
-
 
 }
 
@@ -63,8 +65,12 @@ void Busqueda(int arr[],int t,int dato)
    }while (i<=j);
    
    cout<<"\nElemto a buscar "<<dato;
+   binario<<"\nElemto a buscar "<<dato;
+   binario<<endl;
+   cout<<endl;
    cout<<"\nEl elemento si existe  "<<arr[k];
    binario<<"\nEl elemento si existe  "<<arr[k];
+   binario<<endl;
    binario.close();
 }
 
